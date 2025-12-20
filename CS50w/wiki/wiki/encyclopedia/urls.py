10f158d06b20d1ecd34ed07fb74f9f202c871eb2
z-goat <mishmish_lsr@yahoo.com>
2025-12-20 13:59:08 +0000

    Continuation of Project 1: wiki. Added functions for entryand random page

--- CS50w/wiki/wiki/encyclopedia/urls.py
+++ CS50w/wiki/wiki/encyclopedia/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.entry, name="entry"),
+        path("random/", views.random_page, name="random_page"),
 ]
--- CS50w/wiki/wiki/encyclopedia/views.py
+++ CS50w/wiki/wiki/encyclopedia/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+import markdown
+import random
 from . import util
 
 
@@ -9,7 +10,34 @@ def index(request):
     })
 
 def entry(request, title):
-    content = util.get_entry(title)
     
-    if content is None:
-        return render("encyclopedia/error.html", )
+    entries = util.list_entries()
+    
+    actual_title = None
+    
+    for i in entries:
+        if title.lower() == i.lower():
+            actual_title = i
+            break
+    
+    if actual_title is None:
+        return render(request, "encyclopedia/error.html", {
+            "message": "The requested page was not found",
+            "code": "404",
+        })
+    
+    content = util.get_entry(actual_title)
+    
+    html_content = markdown.markdown(content)
+    
+    return render(request, "encyclopedia/entry.html", {
+        "title": title,
+        "html_content": html_content,
+    })
+    
+def random_page(request):
+    entries = util.list_entries()
+    
+    random_title = random.choice(entries)
+    
+    return redirect("entry", title=random_title)

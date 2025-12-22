3e3b3d0733d95a263401092d2e7baeda35dadf2c
z-goat <mishmish_lsr@yahoo.com>
2025-12-22 21:28:56 +0000

    initial features

--- CS50w/commerce/auctions/models.py
+++ CS50w/commerce/auctions/models.py
@@ -1,6 +1,35 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
 class User(AbstractUser):
     pass
+
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
+
+class Listing(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.TextField()
+    starting_bid = models.DecimalField(max_digits=10, decimal_places=2)
+    current_price = models.DecimalField(max_digits=10, decimal_places=2)
+    image_url = models.URLField(blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True, related_name="listings")
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
+    is_active = models.BooleanField(default=True)
+    watchlist = models.ManyToManyField(User, blank=True, related_name="watched_items")
+
+    def __str__(self):
+        return self.title
+
+class Bid(models.Model):
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bids")
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="bids")
+
+class Comment(models.Model):
+    content = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="comments")
--- CS50w/commerce/auctions/views.py
+++ CS50w/commerce/auctions/views.py
@@ -1,25 +1,26 @@
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse
 
-from .models import User
+from .models import User, Listing, Bid
 
 
 def index(request):
-    return render(request, "auctions/index.html")
-
+    # Fetch only listings that haven't been closed
+    listings = Listing.objects.filter(is_active=True)
+    return render(request, "auctions/index.html", {
+        "listings": listings
+    })
 
 def login_view(request):
     if request.method == "POST":
 
-        # Attempt to sign user in
         username = request.POST["username"]
         password = request.POST["password"]
         user = authenticate(request, username=username, password=password)
 
-        # Check if authentication successful
         if user is not None:
             login(request, user)
             return HttpResponseRedirect(reverse("index"))
@@ -41,7 +42,6 @@ def register(request):
         username = request.POST["username"]
         email = request.POST["email"]
 
-        # Ensure password matches confirmation
         password = request.POST["password"]
         confirmation = request.POST["confirmation"]
         if password != confirmation:
@@ -49,7 +49,6 @@ def register(request):
                 "message": "Passwords must match."
             })
 
-        # Attempt to create new user
         try:
             user = User.objects.create_user(username, email, password)
             user.save()
@@ -61,3 +60,59 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+    
+from django.contrib.auth.decorators import login_required
+from .forms import ListingForm
+
+@login_required 
+def create_listing(request):
+    if request.method == "POST":
+        form = ListingForm(request.POST)
+        if form.is_valid():
+            listing = form.save(commit=False)
+            listing.owner = request.user
+            listing.current_price = listing.starting_bid 
+            listing.save()
+            return redirect("index")
+    else:
+        form = ListingForm()
+    
+    return render(request, "auctions/create.html", {
+        "form": form
+    })
+
+
+def listing_page(request, listing_id):
+    listing = Listing.objects.get(pk=listing_id)
+    
+    is_watched = False
+    if request.user.is_authenticated:
+        if listing.watchlist.filter(id=request.user.id).exists():
+            is_watched = True
+
+    if request.method == "POST":
+        if "place_bid" in request.POST:
+            bid_amount = float(request.POST['bid_amount'])
+            
+            if bid_amount > listing.current_price:
+                listing.current_price = bid_amount
+                listing.save()
+                Bid.objects.create(user=request.user, listing=listing, amount=bid_amount)
+                return redirect("listing", listing_id=listing.id)
+            else:
+                return render(request, "auctions/listing.html", {
+                    "listing": listing,
+                    "is_watched": is_watched,
+                    "error": "Bid must be higher than current price."
+                })
+        if "watchlist_add" in request.POST:
+            listing.watchlist.add(request.user)
+            return redirect("listing", listing_id=listing.id)
+        elif "watchlist_remove" in request.POST:
+            listing.watchlist.remove(request.user)
+            return redirect("listing", listing_id=listing.id)
+
+    return render(request, "auctions/listing.html", {
+        "listing": listing,
+        "is_watched": is_watched
+    })

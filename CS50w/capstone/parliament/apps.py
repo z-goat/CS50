e6ea773118941fe14eb09cbae827e8fda7001f76
z-goat <mishmish_lsr@yahoo.com>
2026-01-10 21:22:43 +0000

    decided to leave CS50W

--- CS50w/capstone/parliament/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ParliamentConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'parliament'
--- CS50w/commerce/auctions/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import User, Listing, Bid, Comment, Category
-
-admin.site.register(User)
-admin.site.register(Listing)
-admin.site.register(Bid)
-admin.site.register(Comment)
-admin.site.register(Category)
